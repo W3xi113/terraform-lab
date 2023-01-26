26730cc4822d3ff90d807281502af57d17284b14
W3xi113 <123630625+W3xi113@users.noreply.github.com>
2023-01-26 12:48:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,8 +44,8 @@ resource "ibm_is_subnet" "subnet2" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance1" {
-  name    = "instance1"
+resource "ibm_is_instance" "instance1alv" {
+  name    = "instance1alv"
   image   = var.image
   profile = var.profile
   primary_network_interface {
@@ -58,8 +58,8 @@ resource "ibm_is_instance" "instance1" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance2" {
-  name    = "instance2"
+resource "ibm_is_instance" "instance2alv" {
+  name    = "instance2alv"
   image   = var.image
   profile = var.profile
   primary_network_interface {
